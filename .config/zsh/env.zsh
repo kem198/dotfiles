d3d39050c53a7f7c9b53a96d4cd7f77571b10b66
kenkenpa198 <kenkenpa198@gmail.com>
2025-04-15 22:53:17 +0900

    add SDKMAN initialization to zsh environment configuration

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -63,6 +63,14 @@ eval "$(rbenv init - zsh)"
 
 
 ########################################
+# SDKMAN!
+########################################
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+
+########################################
 # zsh-autosuggestions
 ########################################
 # 候補をデフォルトより暗く表示する
